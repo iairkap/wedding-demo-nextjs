f88da2bc1c5085a0d90071414bab1d3a1c75e0e2
iair kaplun <iairkap@gmail.com>
2024-04-02 00:06:33 -0300

    push

--- src/app/components/WhenWhereCard.tsx
+++ src/app/components/WhenWhereCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Diviser from './Diviser';
 import styles from "../styles/WhenWhereCard.module.css"
+import Image from 'next/image';
 interface Props {
     title: string;
     parraf: string;
@@ -13,11 +14,14 @@ function WhenWhereCard({ title, parraf, parraf2 }: Props): JSX.Element {
     return (
 
         <section className={styles.section}>
+
             <header className={styles.header}>
-                <div className={styles["red-mon"]}>
-                    <h2 className={styles.title}>{title}</h2>
+                <div className={styles.absolute}>
+                    <Image src={"/etiqueta.png"} width={500} height={300} layout="responsive" alt={title} />
                 </div>
+
             </header>
+            <h2 className={styles.title}>{title}</h2>
             <div className={styles["parraf-container"]}>
                 <p className={styles.p}>{parraf}</p>
                 {parraf2 && <p className={styles.p}>{parraf2}</p>}
